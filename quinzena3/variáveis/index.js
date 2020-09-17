1ba13720af32dbd374c82519f38d3b4ff76825f5
jonnathanvieira <jonnathan.vieira2@gmail.com>
2020-09-17 20:16:58 -0300

    variáveis

---
+++ quinzena3/variáveis/index.js
@@ -0,0 +1,63 @@
+/* Exercício 1 - o que será impresso no console será 10 e 10 5 */
+
+/* Exercício 2 - o que será impresso no console será 10 10 10 */
+
+let nome
+
+let idade
+
+typeof nome
+
+typeof idade
+
+/* O tipo da variável que foi impresso foi undefined, ou seja, significa que
+este tipo a falta de um valor de uma variável */
+
+let nome = prompt("Qual é o seu nome?")
+
+let idade = prompt("Qual é a sua idade?)
+
+/* Notei que foi perguntado o nome e a idade, mas não foi impresso na tela o que foi digitado. */
+
+let nome = "Jonnathan"
+
+let idade = 34
+
+console.log("Olá", nome, "você tem", idade, "anos")
+
+var nome = "Jonnathan"
+var idade = 34
+var nome = prompt("Qual é o seu nome?")
+console.log("Meu nome é", nome)
+
+var cidade = "Teófilo Otoni"
+var cidade = prompt("Qual é a sua cidade?")
+console.log("Minha cidade é", cidade)
+
+var idade = 34
+var idade = prompt("Qual é a sua idade?")
+console.log("Minha idade é", idade)
+
+var fruta = maçã
+var fruta = prompt("Qual é a sua fruta favorita?")
+console.log("Minha fruta favorita é", fruta)
+
+var estado = "Minas Gerais"
+var estado = prompt("Qual Estado você mora?")
+console.log("Eu moro no Estado de", estado)
+
+let comidas = ["macarrão", "arroz", "feijão", "sopa", "legumes"]
+console.log(comidas)
+console.log("Essas são minhas comidas preferidas:")
+console.log(comidas[0])
+console.log(comidas[1])
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4])
+
+let perguntas = ["A maçã é verde?", "O animal é azul?", "A impressora é vermelha?"]
+let respostas = [false, true, false]
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
+
