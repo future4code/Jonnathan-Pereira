df30124ef212e9f635882a248ceda10ed6ac8307
jonnathanvieira <jonnathan.vieira2@gmail.com>
2020-09-27 09:24:03 -0300

    projeto-brackjack

---
+++ quinzena3/blackjack/nossoJogo.js
@@ -0,0 +1,49 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+/* Exercício 1 
+
+console.log("Bem vindo ao jogo de Blackjack!")
+
+*/
+
+/* Exercício 2 
+
+confirm("Quer iniciar uma nova rodada?")
+
+*/
+
+/* Exercício 3 
+
+
+
+if(confirm("Quer iniciar uma nova rodada?") === false) {
+   console.log("O jogo acabou")
+
+}
+
+*/
+
+/* Exercício 4 
+
+if(confirm("Quer iniciar uma nova rodada?") === false) {
+   console.log("O jogo acabou")
+}
+else {
+   console.log("Iniciar")
+}
+
+*/
+
+/* Exercício 5 */
+
